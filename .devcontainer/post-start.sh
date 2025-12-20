cf57c93b9df4effdf3d4f311a6d62601f7a8372e
Jochen <jochen.roth@b13.com>
2025-12-20 17:32:29 +0000

    Init commit

---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/zsh
+
+WORKSPACE_PATH=/workspace
+
+sudo chmod a+x "$(pwd)"
+
+if [ -z "$(ls -A $WORKSPACE_PATH)" ]; then
+    echo "The folder $WORKSPACE_PATH is not empty, skipping Santa's Little Helper setup."
+    exit 1
+else
+    SLH_USERNAME=ochorocho SLH_HOOK_CREATE=1 SLH_COMMIT_TEMPLATE="~/.santas-little-helper/gitmessage.txt" slh core:setup /workspace --no-interaction
+fi
+
+# Add composer bin directory to PATH
+# so all commands are globally available
+echo "export PATH=$WORKSPACE_PATH/bin:\$PATH" >> ~/.bashrc
+
+# Dynamically set TYPO3_BASE_DOMAIN depending on the environment (local or codespaces)
+if [[ -n "$CODESPACE_NAME" ]]; then
+  baseDomain="export TYPO3_BASE_DOMAIN=https://$CODESPACE_NAME-8884.app.github.dev"
+  echo "$baseDomain" >> ~/.zshrc
+else
+  baseDomain="export TYPO3_BASE_DOMAIN=http://127.0.0.1:8884"
+  echo "$baseDomain" >> ~/.zshrc
+fi
+
+# Add scheduler cron
+echo "* * * * * root /usr/local/bin/slh php $WORKSPACE_PATH/vendor/bin/typo3 scheduler:run > /proc/1/fd/1 2>/proc/1/fd/2" | sudo tee /etc/cron.d/typo3-scheduler
+sudo chmod 0644 /etc/cron.d/typo3-scheduler
+
+# Mind the order of the services to start
+sudo service cron start
+
+# Ensure caches are clean and env vars will be loaded
+slh php $WORKSPACE_PATH/vendor/bin/typo3 extension:setup
+slh php $WORKSPACE_PATH/vendor/bin/typo3 cache:flush
