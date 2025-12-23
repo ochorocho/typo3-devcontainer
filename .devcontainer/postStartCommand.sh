dc6d96018b29c711907d142dd5197edabd71ff0e
Jochen <jochen.roth@b13.com>
2025-12-23 12:52:10 +0100

    [TASK] Get codespace to work

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -13,6 +13,10 @@ sudo service cron start
 # Ensure caches are clean and env vars will be loaded
 slh php $WORKSPACE_PATH/vendor/bin/typo3 extension:setup
 slh php $WORKSPACE_PATH/vendor/bin/typo3 cache:flush
+
+# Ugly hack to make the webserver run, @todo: fix the binary installation
+sudo rm -f /home/vscode/.santas-little-helper/bin/frankenphp
+
 nohup bash -c "cd $WORKSPACE_PATH && slh webserver &"
 
 # code --reuse-window .devcontainer/DEVCONTAINER-INFO.md
